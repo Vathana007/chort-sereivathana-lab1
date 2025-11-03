bc74ef3baa8dfddfda52be66d6767cddb57f41c2
Chort Sereivathana <chortsereivathana@gmail.com>
2025-11-03 15:14:10 +0700

    Update CI/CD message

--- test/test.js
+++ test/test.js
@@ -2,9 +2,9 @@ import request from 'supertest';
 import app from '../app.js';
 import { expect } from 'chai';
 describe('GET /', () => {
-    it('should return Hello, GitHub Actions!', async () => {
+    it('should return Hello, CI/CD!', async () => {
         const res = await request(app).get('/');
         expect(res.status).to.equal(200);
-        expect(res.text).to.equal('Hello, GitHub Actions!');
+        expect(res.text).to.equal('Hello, CI/CD!');
     });
 });
